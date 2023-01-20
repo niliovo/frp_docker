51260ad0723374e23313417dc332f70ae9e59bbe
niliovo <1791356563@qq.com>
2023-01-20 19:17:27 +0800

    update

--- dockerfile/frp_build.sh
+++ dockerfile/frp_build.sh
@@ -17,9 +17,9 @@ v_n=${v#*v}
 
 pf=$(dpkg --print-architecture|grep -v musl-linux-|grep -v i|grep -v hf)
 
-f_v=frp_"$v_n"_linux_"$pf"
+f_v=frp_"$v_n"_linux_'$pf'
 
-tg_v=frp_"$v_n"_linux_"$pf".tar.gz
+tg_v=frp_"$v_n"_linux_'$pf'.tar.gz
 
 mkdir -p $sc_d
 
