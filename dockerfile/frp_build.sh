2a9d2605bc4aa05bbe657031de064f7fa6153e21
niliovo <1791356563@qq.com>
2023-01-20 20:04:40 +0800

    update

--- dockerfile/frp_build.sh
+++ dockerfile/frp_build.sh
@@ -10,8 +10,6 @@ apk --update --no-cache add openssl tzdata curl tar wget dpkg grep
 
 pf="$(dpkg --print-architecture|sed 's/^.*musl-linux-//g'|sed 's/hf.*$//g'|sed 's/^.*i//g')"
 
-echo "$pf"
-
 v=$(wget -qO- -t1 -T2 "https://api.github.com/repos/fatedier/frp/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'
 )
 
