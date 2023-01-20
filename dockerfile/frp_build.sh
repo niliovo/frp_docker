42ed41faee20af501345033b3d19443e0f762e5d
niliovo <1791356563@qq.com>
2023-01-20 18:16:20 +0800

    update

--- dockerfile/frp_build.sh
+++ dockerfile/frp_build.sh
@@ -8,6 +8,13 @@ sed -i 's/dl-cdn.alpinelinux.org/mirrors.ustc.edu.cn/g' /etc/apk/repositories
 
 apk --update --no-cache add openssl tzdata curl tar wget dpkg
 
+rm -rf /tmp/*
+
+v=$(wget -qO- -t1 -T2 "https://api.github.com/repos/fatedier/frp/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'
+)
+
+v_n=${v#*v}
+
 p=$(dpkg --print-architecture|grep -v musl-linux-)
 
 if [ $p="amd64" ]; then
@@ -19,29 +26,6 @@ elif [ $p="arm64" ]; then
 elif [ $p="armhf" ]; then
 	pf=arm
 fi
-
-rm -rf /tmp/*
-
-v=$(wget -qO- -t1 -T2 "https://api.github.com/repos/fatedier/frp/releases/latest" | grep "tag_name" | head -n 1 | awk -F ":" '{print $2}' | sed 's/\"//g;s/,//g;s/ //g'
-)
-
-v_n=${v#*v}
-
-p=$(dpkg --print-architecture|grep -v musl-linux-)
-
-echo $p
-
-if [ p=="amd64" ]; then
-	pf=amd64
-elif [ p=="i386" ]; then
-	pf=386
-elif [ p=="arm64" ]; then
-	pf=arm64
-elif [ p=="armhf" ]; then
-	pf=arm
-fi
-
-echo $pf
 
 f_v=frp_"$v_n"_linux_"$pf"
 
